6eedf89c990fc9dd0faa51ec803d0a2078705a2e
ernie <ernie@192.168.1.167>
2015-07-15 17:26:38 +0100

    test

--- src/antproject/AntProject.java
+++ src/antproject/AntProject.java
@@ -19,7 +19,9 @@ public class AntProject {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World");
-        
+        try{
+        }catch(Exception E){
+        }
     }
     
 }
